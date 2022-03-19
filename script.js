63c6e0501d87929606fd5cb9261dfd678dfd450f
wolf-ed <wolf7ed@gmail.com>
2022-03-19 20:59:46 +0100

    finished

--- script.js
+++ script.js
@@ -4,19 +4,9 @@ const main = document.getElementById('main')
 const form = document.getElementById('user-form')
 const search = document.getElementById('search')
 
-async function getUser(userName) {
-    try {
-        const { data } = await axios.get(APIURL + userName);
-
-        createUserCard(data)
-    } catch (err) {
-        console.log(err)
-    }
-
-}
 
 const createUserCard = (user) => {
-    const cardHTML = `  <div class="card">
+    const cardHTML = `<div class="card">
     <div>
         <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
     </div>
@@ -35,6 +25,52 @@ const createUserCard = (user) => {
 
     main.innerHTML = cardHTML
 
+}
+
+const createErrorCard = (errorText) => {
+    const cardHTML = `
+<div class="card">
+<h1>${errorText}</h1>
+</div>
+`
+    main.innerHTML = cardHTML;
+}
+
+async function getUser(userName) {
+    try {
+        const { data } = await axios.get(APIURL + userName);
+
+        createUserCard(data);
+        getRepos(userName);
+    } catch (err) {
+        if (err.response.status == 404) {
+            createErrorCard(`No profile with this username: ${userName}`)
+        }
+    }
+}
+
+async function getRepos(userName) {
+    try {
+        const { data } = await axios.get(APIURL + userName + '/repos?sort=created');
+
+        addReposToCard(data);
+    } catch (err) {
+        createErrorCard(`Problem fetching repos`);
+    }
+}
+
+const addReposToCard = (repos) => {
+    const reposEl = document.getElementById('repos');
+    repos.slice(0, 10)
+        .forEach(repo => {
+            const repoEl = document.createElement('a');
+            repoEl.classList.add('repo');
+            repoEl.href = repo.html_url;
+            repoEl.target = '_blank'
+            repoEl.innerHTML = repo.name;
+
+            reposEl.appendChild(repoEl);
+        })
 }
 
 form.addEventListener('submit', (e) => {
